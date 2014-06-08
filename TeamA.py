6a6a1f4d3118831a0b763c1a013dc2c1546097dc
nikhatkarimi <nikhatkarimi@gmail.com>
2014-06-08 00:21:40 -0500

    get_possible_moves_added

--- TeamA.py
+++ TeamA.py
@@ -21,27 +21,18 @@ class TeamA:
         self.directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
         # a list of legal moves for both the players
         #  for white sqaures possible moves
-        set_for_white = self.get_possible_moves(3, 4, self.directions, 'W', 'B')
-        set_for_white = set_for_white.union(self.get_possible_moves(4, 3, self.directions, 'W', 'B'))
-        set_for_black = self.get_possible_moves(4, 4, self.directions, 'B', 'W')
-        set_for_black = set_for_black.union(self.get_possible_moves(3, 3, self.directions, 'B', 'W'))
-        self.players_possible_moves = [set_for_black, set_for_white] # black player's list first followed by white one's
-        print ("possible white moves:"+str(self.players_possible_moves[1]))
-
-    def get_possible_moves (self, row, col, directions, player, opp):
-        print ("directions:"+str(directions)+"row:"+str(row)+"col:"+str(col))
+        self.possible_moves = set (((2,2),(2,3),(2,4),(2,5),(3,2),(3,5),(4,2), (4,5), (5,2), (5,3), (5,4), (5,5)))
+	
+    def get_possible_moves (self, row, col):
         possible_moves = set()
-        for i in directions:
+        for i in self.directions:
             check_row = row + i[0]
             check_col = col + i[1]
-            #print "row:"+str(check_row)+"col:"+str(check_col)
-            if (self.board[check_row][check_col]==' '):
-                if self.islegal(check_row, check_col, player, opp):
-                    possible_moves.add((check_row, check_col))
-		#print "possible moves:"+str(possible_moves)
-		print (possible_moves)
+            if (0 <= check_row < self.size and 0 <= check_col < self.size and self.board[check_row][check_col]==' '):
+                possible_moves.add((check_row, check_col))
+		
         return possible_moves
-        #prints the boards
+   
 
     def PrintBoard(self):
         # Print column numbers
@@ -65,6 +56,8 @@ class TeamA:
         #in one of the directions. If the input position is adjacent to an opponents piece, this function looks to see if there is a
         #a chain of opponent pieces in that direction, which ends with one of the players pieces.
 
+
+
     def islegal(self, row, col, player, opp):
         if (self.get_square(row, col) != " "):
             return False
@@ -87,16 +80,17 @@ class TeamA:
                         return True
         return False
 
-            #returns true if the square was played, false if the move is not allowed
 
+
+    #returns true if the square was played, false if the move is not allowed
     def place_piece(self, row, col, player, opp):
         if(row == -1 and col == -1):
-            return False
+	    return False
         if (self.get_square(row, col) != " "):
-            return False
+	    return False
 
         if (player == opp):
-            print("player and opponent cannot be the same")
+            print("player and opponent cannot be the same.........................................................................")
             return False
 
         legal = False
@@ -136,6 +130,10 @@ class TeamA:
     # Place a piece of the opponent's color at (row,col)
          if (row, col) != (-1, -1):
                 self.place_piece(row, col, oppColor, playerColor)
+		# obtain new possible moves surronding the last square played and add them, also delete the last square played
+		self.possible_moves.remove((row, col))
+		self.possible_moves = self.possible_moves.union(self.get_possible_moves(row, col))
+                
                 # Determine best move and and return value to Matchmaker
                 self.r = -1
                 self.c = -1
@@ -144,6 +142,9 @@ class TeamA:
                 # print("|||||||||||||||||||||||||||||||||||||||||||||")
                 self.MaxplyMaster(playerColor,oppColor,-1000,1000,0)
                 self.place_piece(self.r, self.c,playerColor,oppColor)
+		self.possible_moves.remove((self.r, self.c))
+		self.possible_moves = self.possible_moves.union(self.get_possible_moves(self.r, self.c))
+                
                 return (self.r,self.c)
 
             #sets all tiles along a given direction (Dir) from a given starting point (col and row) for a given distance
@@ -161,91 +162,18 @@ class TeamA:
 
             #Search the game board for a legal move, and play the first one it finds
 
-    def make_move(self, playerColor, oppColor):
-            for row in range(self.size):
-                for col in range(self.size):
-                    if (self.islegal(row, col, playerColor, oppColor)):
-                        for Dir in self.directions:
-                            #look across the length of the board to see if the neighboring squares are empty,
-                            #held by the player, or held by the opponent
-                            for i in range(self.size):
-                                if ((( row + i * Dir[0]) < self.size) and (( row + i * Dir[0]) >= 0 ) and (
-                                            ( col + i * Dir[1]) >= 0 ) and (( col + i * Dir[1]) < self.size )):
-                                    #does the adjacent square in direction dir belong to the opponent?
-                                    if self.get_square(row + i * Dir[0], col + i * Dir[1]) != oppColor and i == 1:  # no
-                                        #no pieces will be flipped in this direction, so skip it
-                                        break
-                                    #yes the adjacent piece belonged to the opponent, now lets see if there are a chain
-                                    #of opponent pieces
-                                    if self.get_square(row + i * Dir[0], col + i * Dir[1]) == " " and i != 0:
-                                        break
-
-                                    #with one of player's pieces at the other end
-                                    if self.get_square(row + i * Dir[0],
-                                                       col + i * Dir[1]) == playerColor and i != 0 and i != 1:
-                                        #set a flag so we know that the move was legal
-                                        legal = True
-                                        self.flip_tiles(row, col, Dir, i, playerColor)
-                                        break
-                        return (row, col)
-            return (-1, -1)
-
-    def cornerCheck(self,row,col,directions,player,opp):
-        score = 0
-        #possibleMoves = self.get_possible_moves(row,col,directions,player,opp)
-        playerPossibleMoves = self.FindMoves(player,opp)
-        opponentPossibleMoves = self.FindMoves(opp,player)
-        score = score + len(playerPossibleMoves)-len(opponentPossibleMoves)
-        print ("from corner:")
-        if (2,4) in playerPossibleMoves and (2,4)not in opponentPossibleMoves:
-            score = score +10
-        if (0,7) in playerPossibleMoves and (0,7)not in opponentPossibleMoves:
-            score = score +10
-        if (7,0) in playerPossibleMoves and (7,0)not in opponentPossibleMoves:
-            score = score +10
-        if (7,7) in playerPossibleMoves and (7,7)not in opponentPossibleMoves:
-            score = score +10
-
-        return score
-
-    def evaluation(self,row,col,directions,player,opp):
-        cornerScore = self.cornerCheck(row,col,directions,player,opp)
-
-
+    
 
     def FindMoves(self, playerColor, oppColor):
         moves = []
-        for row in range(self.size):
-            for col in range(self.size):
-                if (self.islegal(row, col, playerColor, oppColor)):
-                    for Dir in self.directions:
-                        #look across the length of the board to see if the neighboring squares are empty,
-                        #held by the player, or held by the opponent
-                        for i in range(self.size):
-                            if ((( row + i * Dir[0]) < self.size) and (( row + i * Dir[0]) >= 0 ) and (
-                                        ( col + i * Dir[1]) >= 0 ) and (( col + i * Dir[1]) < self.size )):
-                                #does the adjacent square in direction dir belong to the opponent?
-                                if self.get_square(row + i * Dir[0], col + i * Dir[1]) != oppColor and i == 1:  # no
-                                    #no pieces will be flipped in this direction, so skip it
-                                    break
-                                #yes the adjacent piece belonged to the opponent, now lets see if there are a chain
-                                #of opponent pieces
-                                if self.get_square(row + i * Dir[0], col + i * Dir[1]) == " " and i != 0:
-                                    break
-
-                                #with one of player's pieces at the other end
-                                if self.get_square(row + i * Dir[0],
-                                                   col + i * Dir[1]) == playerColor and i != 0 and i != 1:
-                                    #set a flag so we know that the move was legal
-                                    legal = True
-                                    #self.flip_tiles(row, col, Dir, i, playerColor)
-                                    break
-                    moves.append((row, col))
+        for move in self.possible_moves:
+                if (self.islegal(move[0], move[1], playerColor, oppColor)):
+                    moves.append((move[0], move[1]))
         if(moves):
-            # print("p")
-            # print(moves)
             return moves
-        return [(-1, -1)]
+	return [(-1, -1)]
+
+
 
     def evaluationfn(self,PlayerColor,oppColor):
         score = 0
@@ -270,43 +198,81 @@ class TeamA:
         return score
 
 
+    # checks if the move you are going to remove in backtracking is a valid move due to some neighbouring square or not, if yes it shouldn't be removed. Means if that move is surrounded by some disc then its a neighbouring sqaure of that disc so its a valid move
+    def find_valid_moves(self, moves):
+	valid_moves = set()
+	for move in moves:
+		for direction in self.directions:
+			check_row = move[0] + direction[0]
+            		check_col = move[1] + direction[1]
+            		if (0 <= check_row < self.size and 0 <= check_col < self.size and self.board[check_row][check_col]!=' '):
+                		valid_moves.add(move)
+				break
 
-
-
-
-
+	return valid_moves		
+	
 
     def MaxplyMaster(self,playerColor,oppColor,alpha,beta,depth):
-            moves = self.FindMoves(playerColor,oppColor)
+	    moves = self.FindMoves(playerColor,oppColor)
             v = -1000
+	    changed = 0
             for move in moves:
                 board = copy.deepcopy(self.board)
-                # board = self.board
-                self.place_piece(move[0],move[1],playerColor,oppColor)
+		if self.place_piece(move[0],move[1],playerColor,oppColor):
+			changed = 1
+			self.possible_moves.remove((move[0],move[1]))
+			new_moves = self.get_possible_moves(move[0], move[1])
+			self.possible_moves = self.possible_moves.union(new_moves)
+			
                 v = self.minply(playerColor,oppColor,alpha,beta,depth+1)
                 if(alpha < v):
                     self.r,self.c = move[0],move[1]
                     alpha = v
-                # self.board = copy.deepcopy(board)
                 self.board = board
+		if changed == 1:
+			changed = 0
+			# backtrack and add the move removed
+			self.possible_moves.add((move[0],move[1]))
+			# fetch the moves which are valid and which should not be removed during backtracking
+			valid_moves = self.find_valid_moves(new_moves)
+			# subtract  the valid moves from the new_moves so they won't be removed from possible_moves set
+			new_moves -= valid_moves
+			# remove the moves which were added based on the move removed previously
+			self.possible_moves -= new_moves
+			
                 if(beta <= alpha):
                     return alpha
             return v
 
 
     def minply(self,playerColor,oppColor,alpha,beta,depth):
-        if(depth >= DEPTHLIMIT):
-            return self.evaluationfn(playerColor,oppColor)
+	if(depth >= DEPTHLIMIT):
+	    return self.evaluationfn(playerColor,oppColor)
         else:
-            moves = self.FindMoves(oppColor,playerColor)
-            v = 1000
+	    moves = self.FindMoves(oppColor,playerColor)
+	    v = 1000
+	    changed = 0
             for move in moves:
-                # board = self.board
                 board = copy.deepcopy(self.board)
-                self.place_piece(move[0],move[1],oppColor,playerColor)
+		if self.place_piece(move[0],move[1], oppColor, playerColor):
+			changed = 1
+			self.possible_moves.remove((move[0],move[1]))
+			new_moves = self.get_possible_moves(move[0], move[1])
+			self.possible_moves = self.possible_moves.union(new_moves)
+			
                 v = min(v,self.maxply(playerColor,oppColor,alpha,beta,depth+1))
-                # self.board = copy.deepcopy(board)
                 self.board = board
+		# backtrack and add the move removed
+	        if changed == 1:
+            		changed = 0
+			self.possible_moves.add((move[0],move[1]))
+			# fetch the moves which are valid and which should not be removed during backtracking
+			valid_moves = self.find_valid_moves(new_moves)
+			# subtract  the valid moves from the new_moves so they won't be removed from possible_moves set
+			new_moves -= valid_moves
+			# remove the moves which were added based on the move removed previously
+			self.possible_moves -= new_moves
+			
                 beta = min(beta,v)
                 if(beta <= alpha):
                     return beta
@@ -314,29 +280,42 @@ class TeamA:
             return v
 
     def maxply(self,playerColor,oppColor,alpha,beta,depth):
-        if(depth >= DEPTHLIMIT):
-            return self.evaluationfn(playerColor,oppColor)
+	if(depth >= DEPTHLIMIT):
+	    return self.evaluationfn(playerColor,oppColor)
         else:
-            moves = self.FindMoves(playerColor,oppColor)
-            v = -1000
+	    moves = self.FindMoves(playerColor,oppColor)
+	    v = -1000
+	    changed = 0
             for move in moves:
                 board = copy.deepcopy(self.board)
-                # board = self.board
-                self.place_piece(move[0],move[1],playerColor,oppColor)
+                if self.place_piece(move[0],move[1],playerColor,oppColor):
+			changed = 1
+			self.possible_moves.remove((move[0],move[1]))
+			new_moves = self.get_possible_moves(move[0], move[1])
+			self.possible_moves = self.possible_moves.union(new_moves)
+			
                 v = max(v,self.minply(playerColor,oppColor,alpha,beta,depth+1))
-                # self.board = copy.deepcopy(board)
                 self.board = board
+		if changed == 1:
+            		changed = 0
+			self.possible_moves.add((move[0],move[1]))
+			# fetch the moves which are valid and which should not be removed during backtracking
+			valid_moves = self.find_valid_moves(new_moves)
+			# subtract  the valid moves from the new_moves so they won't be removed from possible_moves set
+			new_moves -= valid_moves
+			# remove the moves which were added based on the move removed previously
+			self.possible_moves -= new_moves
+			#self.PrintBoard()
+			#print ("old possible moves:"+str(self.possible_moves))
+                
+                
                 alpha = max(alpha,v)
                 if(alpha >= beta):
                     return alpha
             return v
 
 
-#def main():
-    #board = TeamA()
-    #board.PrintBoard()
-    #score = board.cornerCheck(3,4,board.directions,'W','B')
-    #print(score);
+
 def main():
     c = TeamA()
     flag = 'Y'
